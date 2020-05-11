bd997961791c883fadda2e5c90cc0d1c6bb43775
Ayshhh <Ayshhh@github.com>
2020-05-11 23:21:44 +0530

    Add project files.

---
+++ Reminder/Reminder/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Reminder
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Reminder.ItemsSource = Reminds;
+
+        }
+
+        ObservableCollection<Remind> Reminds = new ObservableCollection<Remind>()
+        {
+            new Remind
+            {
+            }
+        }; 
+
+        public void Add(object sender, EventArgs e)
+        {
+            Reminds.Add(new Remind { Header = EHeader.Text, Description = EDiscription.Text });
+        }
+    }
+}
---
+++ Reminder/Reminder/Remind.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reminder
+{
+    public class Remind
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+    }
+}
